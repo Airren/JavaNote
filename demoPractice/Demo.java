56071238f7fc2409fd6a5cc020c9e0cefa1dbf2f
Airren <renqiqiang@outlook.com>
2019-09-18 00:40:13 +0800

    Wed Sep 18 00:40:13 CST 2019

--- demoPractice/Demo.java
+++ demoPractice/Demo.java
@@ -2,7 +2,7 @@
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.text.SimpleDateFormat;
-public class Test {
+public class Demo {
     public static void main(String[] args) {
 		System.out.println();
 		Map<String, String> map = new ConcurrentHashMap<>();
---
+++ examination/src/com/company/test/Main.java
@@ -0,0 +1,9 @@
+package com.company.test;
+
+public class Main {
+    public static void main(String[] args) {
+        Integer a = 100;
+        int b = 100;
+        System.out.println(a==b);
+    }
+}
